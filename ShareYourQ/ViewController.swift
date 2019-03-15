72e64304bae290034a356cab77eeaaf2ffe204e6
Furkan Beyhan <frknbyhn@gmail.com>
2019-03-15 09:32:35 +0300

    Initial Commit

---
+++ ShareYourQ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareYourQ
+//
+//  Created by Furkan Beyhan on 15.03.2019.
+//  Copyright © 2019 Furkan Beyhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
